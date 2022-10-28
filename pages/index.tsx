52b0de0371808ca2101cae281f24c8da121f32e0
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-10-28 19:46:09 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,26 @@ import type { NextPage } from "next";
 import {Header,HomeStatic,Post} from '../Components';
 import {sanityClient} from '../sanity';
 import {PostType} from '../typeing';
+import Head from 'next/head';
 interface Porps{
   posts:[PostType];
 }
 const Index: NextPage<Porps> = ({posts}) => {
   return (
+    <>
+    <Head>
+      <title>Medium2.0</title>
+    </Head>
     <main className="max-w-7xl mx-auto">
       <Header />
       <HomeStatic />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6 ">
           {posts.map((post)=>(
             <Post key={post._id} post={post}/>
-          ))}
+            ))}
         </div>
     </main>
+    </>
   );
 };
 export const getServerSideProps = async () =>{
