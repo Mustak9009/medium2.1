e0acf3d16d26ec56cc22c0a64aeee9f719706234
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-10-28 14:33:41 +0530

    ,

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function Home() {
   return (
-    <div className='bg-green-400'>
+    <div className='bg-green-400 py-4'>
       <h1>Ex</h1>
     </div>    
   )
