a770d305b0b2a1774bb23494cf7f2d6abbbdf44c
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-10-28 20:11:59 +0530

    add image component nextjs

--- Components/Post.tsx
+++ Components/Post.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 import {PostType} from '../typeing';
 import {urlFor} from '../sanity';
+import Image from 'next/image';
 export default function Post({post}:{post:PostType}) {
   return (
     <Link href={`/post/${post.slug.current}`} >
         <div className='group overflow-hidden cursor-pointer border rounded-lg'>
           {/*eslint-disable-next-line @next/next/no-img-element */}
-          <img src={urlFor(post.mainImage).url()!} alt='Blog post' className='h-72 object-cover w-full group-hover:scale-105 transition-transform duration-200 ease-in-out'/>     {/*! -> If image is available */} {/*url() => is a function*/}
+          <Image src={urlFor(post.mainImage).url()!} alt='Blog post' className='h-72 object-cover w-full  group-hover:scale-105 transition-transform duration-200 ease-in-out' width={600} height={500}/>     {/*! -> If image is available */} {/*url() => is a function*/}
           <div className='flex justify-between p-5 bg-white'>
             <section>
               <h2 className='font-bold text-lg'>{post.title}</h2>
