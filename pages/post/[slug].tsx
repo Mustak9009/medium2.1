bad2ae404eaaafdb6e47f66c9eb3b81d52257a85
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-10-28 21:30:39 +0530

    Chnage in comment section and img

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -10,14 +10,15 @@ interface PropsType {
   blogPage: PostType;
 }
 const PostFullBlog: NextPage<PropsType> = ({ blogPage }) => {
-
   return (
     <main>
       <Header />
-      <img
+      <Image
         src={urlFor(blogPage.mainImage).url()!}
         alt={blogPage.title}
         className="w-full h-40 object-cover hidden sm:block"
+        width={281}
+        height={179}
       />
       <article className="max-w-3xl mx-auto p-5">
         <h1 className="text-3xl mt-10">{blogPage.title}</h1>
@@ -25,10 +26,12 @@ const PostFullBlog: NextPage<PropsType> = ({ blogPage }) => {
           {blogPage.description}
         </h2>
         <div className="flex items-center space-x-2">
-          <img
+          <Image
             src={urlFor(blogPage.author.image).url()!}
             alt={blogPage.author.name}
             className="w-12 h-12 rounded-full"
+            width={100}
+            height={100}
           />
           <p className="font-extralight text-sm">
             Post by,{" "}
@@ -75,6 +78,7 @@ const PostFullBlog: NextPage<PropsType> = ({ blogPage }) => {
   );
 };
 function Comment({blogPage}:{blogPage:PostType}){
+  console.log(blogPage);
   return(
       <section className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow shadow-yellow-500 space-y-2">
         <h3 className="text-4xl font-bold">Comment</h3>
